11957b37476ec93ee62f76759e6dea5d08e83118
Mirco Grillo <mirco.grillomg@gmail.com>
2019-04-06 18:01:48 +0200

    Adds ArticleSerializer

---
+++ newsapi/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    body = serializers.CharField()
+    location = serializers.CharField()
+    publication_date = serializers.DateField()
+    draft = serializers.BooleanField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Creates and returns a new Article instance based on validated_data
+        """
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Updates and returns a new updated
+        Article instance based on validated_data
+        """
+        instance.author = validated_data.get("author", instance.author)
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.body = validated_data.get("body", instance.body)
+        instance.location = validated_data.get("location", instance.location)
+        instance.draft = validated_data.get("draft", instance.draft)
+        instance.publication_date = validated_data.get(
+            "publication_date", instance.publication_date
+        )
+
+        instance.save()
+        return instance
